7507eedcb7b09a2be2af1d54547e2f7bb706deac
FaDoAdamSandler <99231788+FaDoAdamSandler@users.noreply.github.com>
2022-08-29 08:19:41 -0300

    Add files via upload

--- src/components/CalculadoraComp.vue
+++ src/components/CalculadoraComp.vue
@@ -2,23 +2,23 @@
 export default {
   data() {
     return {
-      qualidades: [
-        { qualidade: 5, servico: "5% (Péssimo)" },
-        { qualidade: 10, servico: "10% (Ruim)" },
-        { qualidade: 15, servico: "15% (OK)" },
-        { qualidade: 20, servico: "20% (Bom)" },
-        { qualidade: 25, servico: "25% (Mais que bom)" },
-        { qualidade: 30, servico: "30% (Excepcional)" },
+      quals: [
+        { qual: 5, servico: "5% (Péssimo)" },
+        { qual: 10, servico: "10% (Ruim)" },
+        { qual: 15, servico: "15% (OK)" },
+        { qual: 20, servico: "20% (Bom)" },
+        { qual: 25, servico: "25% (Mais que bom)" },
+        { qual: 30, servico: "30% (Excepcional)" },
       ],
       valor: 0,
       pessoa: 1,
-      qualidade_selecionada: "",
+      qual_selecionada: "",
     };
   },
   computed: {
     valor_final() {
       return (
-        (this.valor * (1 + this.qualidade_selecionada / 100)) /
+        (this.valor * (1 + this.qual_selecionada / 100)) /
         this.pessoa
       ).toFixed(2);
     },
@@ -28,7 +28,7 @@ export default {
 <template>
   <main>
     <div class="calc">
-      <h1>Calculadora de <strong>Gorjetas</strong></h1>
+      <h1>Calculadora de Gorjetas</h1>
       <div class="valores-forms">
         <label for="input-valor">Qual o valor da conta?</label>
         <input v-model="valor" placeholder="0" id="input-valor" type="number" />
@@ -36,18 +36,18 @@ export default {
       <div class="valores-forms">
         <label for="select-servico">Como foi o serviço?</label>
         <select
-          v-model="qualidade_selecionada"
-          name="select-qualidade"
+          v-model="qual_selecionada"
+          name="select-qual"
           id="select-servico"
         >
           <option disabled value="">Escolha um item</option>
           <option
-            v-for="qualidade of qualidades"
-            :key="qualidade.qualidade"
+            v-for="qual of quals"
+            :key="qual.qual"
             for="select-servico"
-            :value="qualidade.qualidade"
+            :value="qual.qual"
           >
-            {{ qualidade.servico }}
+            {{ qual.servico }}
           </option>
         </select>
       </div>
